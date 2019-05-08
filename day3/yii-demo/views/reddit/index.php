546670012dc318893ae9700b323acf0690b6c27b
Noemi Csaszari <csaszari.noemi@gmail.com>
2019-05-08 17:59:03 +0200

    feat(fix):vote

--- day3/yii-demo/views/reddit/index.php
+++ day3/yii-demo/views/reddit/index.php
@@ -1,10 +1,12 @@
 <h2>Reddit</h2>
 <br>
+
+<a href="http://localhost:8080/reddit/submit" class="btn btn-primary btn-lg">Add a new post</a>
 <?php 
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm; 
-    
+ 
  foreach ($posts as $post): ?>
 
 <div class="card">
@@ -16,13 +18,12 @@ use yii\widgets\ActiveForm;
       <p><?= $post->content?></p>
       <footer class="blockquote-footer">Posted by <cite title="Source Title"><?= $post->creator?></cite></footer>
       <br>
-      <a href="/reddit/upvote?id=<?= $post->id ?>">Vote Up</a>
+      <a href="/reddit/upvote?id=<?= $post->id ?>"class= "btn btn-success">Vote Up</a>
       <p><?= $post->votes?></p>
-      <a href="/reddit/downvote?id=<?= $post->id ?>">Vote Down</a>
+      <a href="/reddit/downvote?id=<?= $post->id ?>" class= "btn btn-danger">Vote Down</a>
     </blockquote>
   </div>
 </div>
 <br>
 
   <?php endforeach; ?>
-  <a href="http://localhost:8080/reddit/submit">Add a new post</a>
